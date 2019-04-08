578e0c78cb96d07f1e8515e95a39f6b24c197cbb
Phlix <819108840@qq.com>
2019-04-08 10:28:23 +0800

    Update cpu_extract_info.py
    add logfile_processing function for worker logfile and ps logfile processing

--- cpu_extract_info.py
+++ cpu_extract_info.py
@@ -3,13 +3,32 @@ from google.protobuf import text_format
 from LogRecord import *
 from LogRecordMgr import LogRecordMgr
 import re
+def logfile_processing(startline, logfile_path, log_record_mgr):
+    line_count = 0
+    with open(logfile_path, 'r') as f:
+        line = f.readline()
+        while line:
+            if line_count>=startline:
+                raw_message = ""
+                ismatch = re.match(r'(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{6}):.*]', line)
+                if ismatch != None:
+                    while line:
+                        line = line[0:-1]+" "
+                        raw_message += line
+                        line = f.readline()
+                        is_newrecord = re.match(r'(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{6}):.*]', line)
+                        if is_newrecord != None:
+                            break
+                    log_record_mgr.parse_log_message(raw_message)
+                    continue
+            line_count += 1
+            line = f.readline()
 
-def worker_logfile_processing(worker_logfile_path):
+def worker_logfile_processing(worker_logfile_path, log_record_mgr):
     """
     get graph and process the logfile
     return graph_pb2.GraphDef() and processed log record
     """
-    log_record_mgr = LogRecordMgr()
     total_graph_str = ""
     graph_end_line = 0
     graph = ["",""]
@@ -35,32 +54,17 @@ def worker_logfile_processing(worker_logfile_path):
     total_graph_str = graph[0] + graph[1]
     graph_def = graph_pb2.GraphDef()
     text_format.Merge(total_graph_str, graph_def)
-    line_count = 0
-    with open(worker_logfile_path, 'r') as f:
-        line = f.readline()
-        while line:
-            if line_count>graph_end_line:
-                raw_message = ""
-                ismatch = re.match(r'(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{6}):.*]', line)
-                if ismatch != None:
-                    while line:
-                        line = line[0:-1]+" "
-                        raw_message += line
-                        line = f.readline()
-                        is_newrecord = re.match(r'(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{6}):.*]', line)
-                        if is_newrecord != None:
-                            break
-                    log_record_mgr.parse_log_message(raw_message)
-                    continue
-            line_count += 1
-            line = f.readline()
+    logfile_processing(graph_end_line, worker_logfile_path, log_record_mgr)
     return graph_def
-def ps_logfile_processing():
-    pass
+
+def ps_logfile_processing(ps_logfile_path, log_record_mgr):
+    logfile_processing(0, ps_logfile_path, log_record_mgr)
 
 # following is test
-
-graph_def = worker_logfile_processing("./LenetLog/lenet-logfile-worker0.txt")
+log_record_mgr = LogRecordMgr()
+#graph_def = worker_logfile_processing("./LenetLog/lenet-logfile-worker0.txt", log_record_mgr)
+ps_logfile_processing("./LenetLog/lenet-logfile-ps0.txt", log_record_mgr)
+log_record_mgr.logs_print()
 #for node in graph_def.node:
 #    print(node.name)
 
