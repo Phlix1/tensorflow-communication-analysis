5ee76acc968a63e77fd57075a927bb2a5edfc023
Phlix <819108840@qq.com>
2019-04-13 14:33:13 +0800

    Update StepInfoMgr.py
    get the ratio of communication time

--- StepInfoMgr.py
+++ StepInfoMgr.py
@@ -6,11 +6,23 @@ class StepInforMgr:
         stepid_list = comm_node_mgr.get_stepids()
         for stepid in stepid_list:
             commtime, machine_commtime = comm_node_mgr.get_commtime_by_stepid(stepid)
-            runtime = log_record_mgr.get_runtime_by_stepid(stepid)
+            runtime, execution_count = log_record_mgr.get_runtime_by_stepid(stepid)
             if commtime==False or runtime==False:
                 continue
-            step_info = StepInfo(stepid, runtime, commtime, machine_commtime)
+            step_info = StepInfo(stepid, execution_count, runtime, commtime, machine_commtime)
             self.stepinfo_list.append(step_info)
+    def get_comm_comp_ratio(self):
+        average_ratio = 0.0
+        stepnum = 0
+        for stepinfo in self.stepinfo_list:
+            for key in stepinfo.machine_commtime.keys():
+                if "worker" in key and stepinfo.machine_commtime[key]/stepinfo.runtime<1.0:
+                    average_ratio += stepinfo.machine_commtime[key]/stepinfo.runtime
+                    stepnum += 1
+        average_ratio /= stepnum
+        return average_ratio
+            
+
     def show_steps(self):
         for stepinfo in self.stepinfo_list:
             stepinfo.stepinfo_print()
