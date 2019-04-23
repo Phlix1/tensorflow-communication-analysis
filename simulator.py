ead01522a30bccd0c00d1d9f5c0688483e18b5c3
shiy10 <shiyang7@qq.com>
2019-04-23 08:43:41 +0800

    add simulator.py

---
+++ simulator.py
@@ -0,0 +1,28 @@
+from op import op
+
+# GLOBAL VARIABLES
+# dict for all ops
+opDict = {}
+# dict for all flows
+flowDict = {}
+
+# init the dict
+def initDict():
+    pass
+
+# assign the priorities for all flows
+def assignPri():
+    pass
+
+
+# begin simulate
+def simulate():
+    pass
+
+# main function 
+def main():
+    initDict()
+
+
+if __name__ == "__main__":
+    main()
